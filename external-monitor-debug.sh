e35f534253d9fb9a3e0035880a5060717eef26eb
Le Phu Bui <36240880+lephubui@users.noreply.github.com>
2018-11-28 22:29:04 -0800

    Removed echo

--- external-monitor-debug.sh
+++ external-monitor-debug.sh
@@ -8,11 +8,4 @@
 # Log debug to local0.debug (/var/log/ltm)?
 # Check if a variable named DEBUG exists from the monitor definition
 # This can be set using a monitor variable DEBUG=0 or 1
-if [ -n "$DEBUG" ]
-then
-   if [ $DEBUG -eq 1 ]; then echo "EAV `basename $0`: \$DEBUG: $DEBUG" | logger -p local0.debug; fi
-else
-   # If the monitor config didn't specify debug, enable/disable it here
-   DEBUG=0
-   #echo "EAV `basename $0`: \$DEBUG: $DEBUG" | logger -p local0.debug
-fi
+
