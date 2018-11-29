79dea509e3132e0fe66e70c79bd2f0312009373f
Le Phu Bui <36240880+lephubui@users.noreply.github.com>
2018-11-28 22:37:35 -0800

    Normal file

--- external-monitor-debug.sh
+++ external-monitor-debug.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
- 
-# Save as /usr/bin/monitors/custom_monitor.bash
-# Make executable using chmod 700 custom_monitor.bash
- 
-# Use a custom shell command to perform a health check of the pool member IP address and port
- 
-# Log debug to local0.debug (/var/log/ltm)?
-# Check if a variable named DEBUG exists from the monitor definition
-# This can be set using a monitor variable DEBUG=0 or 1
+simple file
+ljdlsjbsadkasbdsald
+saldbadlakbdakldq
+lakdnksadnasda
+
 
